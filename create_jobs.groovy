445010ed5633e7447bd27b2b5e4deca9611dc5b7
cmangamm <chandu80006@gmail.com>
2022-05-09 14:21:43 +0530

    Create create_jobs.groovy

---
+++ create_jobs.groovy
@@ -0,0 +1,41 @@
+sam = "sampath"
+B_FOLDER = "$sam/Builds"
+T_FOLDER = "$sam/Tests"
+folder("${B_FOLDER}")
+freeStyleJob("${B_FOLDER}/Build1"){
+  steps{
+    shell "echo 'I am Build1'"
+  }
+}
+folder("${T_FOLDER}")
+freeStyleJob("${T_FOLDER}/Test1"){
+  steps{
+    shell "echo 'I am test1'"
+    shell "ls"
+  }
+}
+pipelineJob("$sam/sam_workflow"){
+  def wf_script="""
+pipeline{ 
+  agent any 
+  stages{ 
+    stage('BuildStage'){
+      steps{
+             build "Builds/Build1" 
+      }
+    }
+    stage ('TestStage'){
+       steps{
+              build "Tests/Test1"
+       }
+     }
+  }
+}
+"""
+  definition {
+    cps{
+      script(wf_script)
+      sandbox()
+    }
+  }
+}
