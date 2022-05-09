6662d18862d386ab880fcdbfddade9ccc5b885d7
cmangamm <chandu80006@gmail.com>
2022-05-09 14:39:00 +0530

    Update create_jobs.groovy

--- create_jobs.groovy
+++ create_jobs.groovy
@@ -1,41 +1,18 @@
-sam = "cmr_dontDelete"
-B_FOLDER = "$sam/Builds"
-T_FOLDER = "$sam/Tests"
-folder("${B_FOLDER}")
-freeStyleJob("${B_FOLDER}/Build1"){
-  steps{
-    shell "echo 'I am Build1'"
-  }
-}
-folder("${T_FOLDER}")
-freeStyleJob("${T_FOLDER}/Test1"){
-  steps{
-    shell "echo 'I am test1'"
-    shell "ls"
-  }
-}
-pipelineJob("$sam/sam_workflow"){
-  def wf_script="""
-pipeline{ 
-  agent any 
-  stages{ 
-    stage('BuildStage'){
-      steps{
-             build "Builds/Build1" 
-      }
-    }
-    stage ('TestStage'){
-       steps{
-              build "Tests/Test1"
-       }
-     }
-  }
-}
-"""
-  definition {
-    cps{
-      script(wf_script)
-      sandbox()
+folder('Test2/Synamedia2')
+l= ['buildjob1','buildjob2','testjob1','testjob2']
+
+def createJob(i)
+{
+  freeStyleJob("Test2/Synamedia2/$i")
+  {
+  scm {
+        github('jenkinsci/job-dsl-plugin', 'master')
     }
   }
+}
+
+for (i in l)
+{
+println ("Job: $i")
+createJob(i)
 }
